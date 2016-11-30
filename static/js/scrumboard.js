e26478df8dec26d4db140b71d32b073a3682204a
Rus Ionathan Emanuel <ionathanr@yahoo.co.uk>
2016-11-30 21:08:30 +0200

    Login with angular
    implementing login for the client-side with angular

---
+++ static/js/scrumboard.js
@@ -0,0 +1,41 @@
+(function() {
+	'use strict';
+
+	angular.module('scrumboard.demo', ['ngRoute'])
+		.controller('ScrumboardController', ['$scope', '$http', '$location', ScrumboardController]);
+
+		function ScrumboardController($scope, $http) {
+		$scope.add = function (list, title) {
+			var card = {
+				list: list.id,
+				title: title
+			};
+			$http.post('/scrumboard/cards/', card)
+				.then(function(response){
+					list.cards.push(response.data);
+				},
+				function(){
+					alert('Cpuld not create card');
+				});
+		};
+
+		$scope.logout = function() {
+			$http.get('/auth_api/logout')
+				.then(function() {
+					$location.url('/login');
+				});
+		}
+
+				
+
+		$scope.data = [];
+		$http.get('/scrumboard/lists/').then(function(response){
+			$scope.data = response.data;
+		});
+
+		$scope.sortBy = 'story_points';
+		$scope.reverse = true;
+		$scope.showFilters = false;
+	}
+				
+}());
